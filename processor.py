807b3c4cca1a9519c8ab150d75c3ccd8b2f202ca
AyaanSharma2007 <ayaan007sharma@gmail.com>
2026-02-18 23:23:44 +0530

    initiaited processor

--- processor.py
+++ processor.py
@@ -5,3 +5,9 @@ import access_data_memory
 import ALUandSignExtend
 import RegisterFunction
 import writeData
+PC = 0
+RUNNING = True
+while RUNNING:
+    raw_instruction=fetch.fetch(PC,"Instructions.txt")
+    decoded_instruction=Decode.decode_instruction(raw_instruction)
+    # if(decoded_instruction[])
