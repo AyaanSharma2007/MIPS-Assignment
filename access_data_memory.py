6e0e77ed1dce42d94cc42fc6ea31d97c8327a003
ArushJain-697 <thearushjain3321@gmail.com>
2026-03-25 11:28:04 +0530

    changed the default parameter to memory_data.txt a

--- access_data_memory.py
+++ access_data_memory.py
@@ -1,8 +1,11 @@
-def access_data_memory(address_32bit, file_name="Data.txt"):
+def access_data_memory(address_32bit, file_name="memory_data.txt"):
     byte_address = int(address_32bit, 2)
 
     with open(file_name, 'r') as file:
         memory_lines = [line.strip() for line in file.readlines()]
+
+    if memory_lines and len(memory_lines[0]) == 32:
+        raise ValueError(f"{file_name} is 32bit per line. access_data_memory expects byte-addressable 8-bit memory.")
 
     while len(memory_lines) <= byte_address + 3:
         memory_lines.append("00000000")
