50941a4331f849f3539f5488a53284a55168050b
ArushJain-697 <thearushjain3321@gmail.com>
2026-03-25 15:38:20 +0530

    simplified code

--- ALUandSignExtend.py
+++ ALUandSignExtend.py
@@ -1,6 +1,7 @@
 def sign_extend_16_to_32(binary_str):
     if len(binary_str) != 16:
         return None
+    #basically concatinating first bit of string 16 times
     return binary_str[0] * 16 + binary_str
 
 
@@ -19,9 +20,19 @@ def ALU(operand1_bin, operand2_bin, control_line):
     elif control_line == "011":
         result = val1 - val2
     elif control_line == "100":
-        s_val1 = val1 - (1 << 32) if (val1 >> 31) else val1
-        s_val2 = val2 - (1 << 32) if (val2 >> 31) else val2
-        result = 1 if s_val1 < s_val2 else 0
+        if (val1 >> 31) == 1:          
+            s_val1 = val1 - (1 << 32)  
+        else:
+            s_val1 = val1           
+            
+        if (val2 >> 31) == 1:
+            s_val2 = val2 - (1 << 32)
+        else:
+            s_val2 = val2
+        if s_val1 < s_val2:
+            result = 1
+        else:
+            result = 0
     else:
         return None
     return f'{result & 0xFFFFFFFF:032b}'
